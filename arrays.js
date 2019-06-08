80eec64380a664b85c1c3730de95df11b368ff9b
Fernanda Romero <fernandaromero85@gmail.com>
2019-06-08 22:02:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function destructivelyAddElementToBeginningOfArray (array, element) {
 }
 
 function addElementToEndOfArray (array, element) {
-  
+  return [...array, element]
 }
 
 function accessElementInArray (array, index) {
