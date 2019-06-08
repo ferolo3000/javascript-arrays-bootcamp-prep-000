f332f33a71f7ed1b9dad20e6142df2a9e2ae7bf2
Fernanda Romero <fernandaromero85@gmail.com>
2019-06-08 21:36:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray () {
-  return ["candy"].unshift(chocolateBars)
+function addElementToBeginningOfArray (array, element) {
+  return array.unshift("foo")
   
 }
 
