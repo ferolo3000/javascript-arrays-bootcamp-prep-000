2b424fb2c9f22021d4e529bc482f4c390eb162b7
Fernanda Romero <fernandaromero85@gmail.com>
2019-06-08 22:02:42 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,7 +13,9 @@ function addElementToEndOfArray (array, element) {
   return [...array, element]
 }
 
-function 
+function destructivelyAddElementToEndOfArray (array, element) {
+  
+}
 
 function accessElementInArray (array, index) {
   return array[index]
